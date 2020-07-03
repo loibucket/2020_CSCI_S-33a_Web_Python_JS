9bc4a0a77f9d28254a1ac7947b73dba050920e17
Loi <loibucket@users.noreply.github.com>
2020-07-03 18:03:58 -0400

    Project 1 Wiki

---
+++ proj1_wiki/encyclopedia/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views, util
+
+urlpatterns = [  
+    path("", views.index, name="index"),
+    path("search", views.search, name="search"), 
+    path("new_page", views.new_page, name="new_page"),       
+    path("edit_page", views.edit_page, name="edit_page"),    
+    path("make", views.make, name="make"),
+    path("random_page", views.random_page, name="random_page"),    
+
+    #this must be last in list
+    path("<str:title>", views.wiki, name="get_entry")    
+]
