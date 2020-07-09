33b20b636d2cca1ec157157ce3ff30a68c4e566c
Loi <loibucket@users.noreply.github.com>
2020-07-08 21:41:50 -0400

    Proj2

--- proj2_commerce/auctions/views.py
+++ proj2_commerce/auctions/views.py
@@ -61,3 +61,9 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def create_listing(request):
+    if request.method == "POST":
+        return
+    else:
+        return render(request, "auctions/create_listing.html")
