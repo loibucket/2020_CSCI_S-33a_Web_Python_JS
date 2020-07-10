c05c312bb16cc589c22a05df8aeda5b7a03d6090
Loi <loibucket@users.noreply.github.com>
2020-07-10 15:20:58 -0400

    Proj 2

--- proj2_commerce/auctions/models.py
+++ proj2_commerce/auctions/models.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # We’ve started you with a User model that represents each user of the application.
 # Because it inherits from AbstractUser, it will already have fields for a username,
 # email, password, etc., but you’re welcome to add new fields to the User class if
 # there is additional information about a user that you wish to represent.
+
 class User(AbstractUser):
-    first_name = models.IntegerField(null=True)
-    last_name = models.IntegerField(null=True)
+    pass
 
 # Models: Your application should have at least three models in addition to the
 # User model: one for auction listings, one for bids, and one for comments made
@@ -16,20 +17,24 @@ class User(AbstractUser):
 # models if you would like.
 
 class Listing(models.Model):
-    auction_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=64)
-    description = models.CharField(max_length=300)
-    image = models.URLField(max_length=200)
+    description = models.CharField(max_length=9000)
+    image_url = models.URLField(max_length=900)
+    is_active = models.BooleanField(default=False)
+    starting_bid = models.DecimalField(max_digits=5, decimal_places=2)
+    current_bid = models.DecimalField(max_digits=5, decimal_places=2)
+    seller = models.ForeignKey('User', on_delete=models.PROTECT)
 
 # bids
 # 2 digit decimal
 class Bid(models.Model):
-    auction_id = models.AutoField(primary_key=True)
-    start_bid = models.DecimalField(max_digits=9, decimal_places=2)
-    curr_bid = models.DecimalField(max_digits=9, decimal_places=2)
+    item_number = models.ForeignKey(Listing, on_delete=models.PROTECT)
+    bidder = models.ForeignKey(User, on_delete=models.PROTECT)
+    bid_amount = models.DecimalField(max_digits=5, decimal_places=2)
     n_bids = models.IntegerField()
 
 # comments
 class Comment(models.Model):
-    auction_id = models.AutoField(primary_key=True)
-    comment = models.CharField(max_length=300)
+    item_number = models.ForeignKey(Listing, on_delete=models.PROTECT)
+    commenter = models.ForeignKey(User, on_delete=models.PROTECT)
+    comment = models.CharField(max_length=9000)
--- proj2_commerce/auctions/views.py
+++ proj2_commerce/auctions/views.py
@@ -4,7 +4,10 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
+from django.contrib.auth.decorators import login_required
+
 from .models import User
+from .models import Listing
 
 
 def index(request):
@@ -62,8 +65,39 @@ def register(request):
     else:
         return render(request, "auctions/register.html")
 
+
+@login_required()
 def create_listing(request):
     if request.method == "POST":
-        return
+        title = request.POST["title"]
+        description = request.POST["description"]
+        image_url = request.POST["image_url"]
+        starting_bid = request.POST["starting_bid"]
+        seller = request.user
+        print(title, description, starting_bid, image_url, seller)
+
+        #listing = Listing(title=title, description=description, image_url=image_url,
+        #                  is_active=True, starting_bid=starting_bid, current_bid=starting_bid, seller=seller)
+
+        #listing.save()
+
+        # Attempt to create new listing
+        try:
+            listing = Listing(title=title, description=description, image_url=image_url,
+                              is_active=True, starting_bid=starting_bid, current_bid=starting_bid, seller=seller)
+            listing.save()
+            return render(request, "auctions/create_listing.html", {
+                "message": "Listing created",
+            })
+        except:
+            return render(request, "auctions/create_listing.html", {
+                "message": "Input Error: Please check fields again",
+                "title": title,
+                "description": description,
+                "image_url": image_url,
+                "starting_bid": starting_bid
+            })
+
+        return render(request, "auctions/create_listing.html")
     else:
         return render(request, "auctions/create_listing.html")
